7c8a41c772c00fb2ef69fb391ecdafc5efd61c9e
NachoYuste <IgnacioYusteL@live.com>
2023-03-29 17:21:18 +0200

    Añadida librería Sharepoint Cliente y creada clase controlador de sharepoint

---
+++ GetDocsExpediente/SharepointController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetDocsExpediente
+{
+    class SharepointController
+    {
+        private string User = ConfigurationManager.AppSettings["user"];
+        private string Pass = ConfigurationManager.AppSettings["password"];
+        private string Url = ConfigurationManager.AppSettings["SiteURL"];
+
+        private ClientContext context { get; }
+    }
+}
