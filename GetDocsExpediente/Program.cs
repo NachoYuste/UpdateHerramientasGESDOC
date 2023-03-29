aa0a0220e03ad18d678b0e72f59cf0d7b98dbde0
NachoYuste <IgnacioYusteL@live.com>
2023-03-29 17:00:09 +0200

    Creación de solución GetDocsExpediente

---
+++ GetDocsExpediente/Program.cs
@@ -0,0 +1,34 @@
+﻿using Serilog;
+using System;
+using System.Configuration;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GetDocsExpediente
+{
+    class Program
+    {
+        public static Serilog.Core.Logger logFile = new LoggerConfiguration().WriteTo.File(AppDomain.CurrentDomain.BaseDirectory + "LOGS\\GetDocsExpediente_LOG_" + DateTime.Now.ToString("dd_MM_yyyy_HH_mm_ss") + ".log").CreateLogger();
+        public static Serilog.Core.Logger logConsole = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+
+        static void Main(string[] args)
+        {
+            string User = ConfigurationManager.AppSettings["user"];
+            string Pass = ConfigurationManager.AppSettings["password"];
+            string Url = ConfigurationManager.AppSettings["SiteURL"];        
+
+
+            //LOGIN
+
+            //GET ID REGISTRO
+
+            //GET LISTA DE DOCUMENTOS / LISTA DE PUNTOS DOCUMENTALES DEL EXPEDIENTE
+
+            //POR CADA PUNTO DOCUMENTAL HACER LLAMADA A GETALLDOCSEXPEDIENTE (SOLUCIÓN YA EXISTENTE)
+        }
+    }
+}
---
+++ GetDocsExpediente/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("GetDocsExpediente")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GetDocsExpediente")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("1d97f905-96bf-49a2-8c37-58084abdf0d0")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
